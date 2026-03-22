8dce18c1e064f25a3feb1750385b81ca1c89485b
kapilsharma05 <kapilgyanvihar@gmail.com>
2026-03-22 13:35:47 +0530

    use WebFlux starter

--- consumeRest/src/main/java/com/cosume/cosumeRest/Controller/ConsumeAPIController.java
+++ consumeRest/src/main/java/com/cosume/cosumeRest/Controller/ConsumeAPIController.java
@@ -5,10 +5,13 @@ package com.cosume.cosumeRest.Controller;
  * URL : https://www.youtube.com/watch?v=9AXAUlp3DBw
 */ 
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,45 +28,73 @@ import jakarta.validation.Valid;
 @RestController
 public class ConsumeAPIController {
 
-	@Autowired
-	private PersonRepository personRepository;
-	
-	@GetMapping("/persons")
-	public List<Person> getAllPersons() {
-		System.out.println("RetryServcie called");
-		System.out.println("get all persone information");
-		return personRepository.findAll();
-	}
+    private final PersonRepository personRepository;
+    private final Logger log = LoggerFactory.getLogger(ConsumeAPIController.class);
 
-	@GetMapping("/person/{personId}")
-	public Person getPersonWithId(@Valid @PathVariable Integer personId) {
-		System.out.println("get person informatino by person id" + personId);
-		return personRepository.findById(personId).get();
-	}
+    // Constructor injection (preferred for testability)
+    public ConsumeAPIController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
-	@PostMapping("/person")
-	public void addPerson(@Valid @RequestBody Person person) {
-		personRepository.save(person);
-		System.out.println("Saving new person information" + person);
-	}
+    @GetMapping("/persons")
+    public List<Person> getAllPersons() {
+        log.info("getAllPersons called");
+        return personRepository.findAll();
+    }
 
-	@PutMapping("/person/{personId}")
-	public void updateOrCreatePerson(@RequestBody Person newPerson, @PathVariable Integer personId) {
-		Optional<Person> person = personRepository.findById(personId);
-		if (person.isPresent()) {
-			Person udpatePerson = person.get();
-			udpatePerson.setAge(newPerson.getAge());
-			udpatePerson.setName(newPerson.getName());
-			personRepository.save(udpatePerson);
-		} else {
-			System.out.println("Please check the person id" + personId);
-		}
-		System.out.println("Updating person information");
-	}
+    @GetMapping("/person/{personId}")
+    public ResponseEntity<Person> getPersonWithId(@PathVariable Integer personId) {
+        log.info("get person information by id={}", personId);
+        return personRepository.findById(personId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-	@DeleteMapping("/person/{personId}")
-	public void addPerson(@Valid @PathVariable int personId) {
-		personRepository.deleteById(personId);
-		System.out.println("delete person information");
-	}
+    @PostMapping("/person")
+    public ResponseEntity<Person> addPerson(@Valid @RequestBody Person person) {
+        Person saved = personRepository.save(person);
+        log.info("Saved new person: {}", saved);
+        // try to include location header if id is present
+        try {
+            Object id = saved.getClass().getMethod("getId").invoke(saved);
+            return ResponseEntity.created(URI.create("/person/" + id)).body(saved);
+        } catch (Exception ex) {
+            return ResponseEntity.ok(saved);
+        }
+    }
+
+    @PutMapping("/person/{personId}")
+    public ResponseEntity<Person> updateOrCreatePerson(@Valid @RequestBody Person newPerson, @PathVariable Integer personId) {
+        Optional<Person> existing = personRepository.findById(personId);
+        if (existing.isPresent()) {
+            Person udpatePerson = existing.get();
+            udpatePerson.setAge(newPerson.getAge());
+            udpatePerson.setName(newPerson.getName());
+            Person saved = personRepository.save(udpatePerson);
+            log.info("Updated person {}", saved);
+            return ResponseEntity.ok(saved);
+        } else {
+            // create new: try to set id if setter exists, otherwise let repo generate
+            try {
+                newPerson.getClass().getMethod("setId", Integer.class).invoke(newPerson, personId);
+            } catch (Exception ignore) {
+                // ignore if no setId method
+            }
+            Person created = personRepository.save(newPerson);
+            log.info("Created person {}", created);
+            return ResponseEntity.created(URI.create("/person/" + personId)).body(created);
+        }
+    }
+
+    @DeleteMapping("/person/{personId}")
+    public ResponseEntity<Void> deletePerson(@PathVariable int personId) {
+        if (personRepository.existsById(personId)) {
+            personRepository.deleteById(personId);
+            log.info("Deleted person id={}", personId);
+            return ResponseEntity.noContent().build();
+        } else {
+            log.warn("Person id={} not found for delete", personId);
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
